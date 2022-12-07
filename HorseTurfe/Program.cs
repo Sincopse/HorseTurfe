60429a7a4b576305a95c8ec955eab43ca414cba9
Simao04 <meloaairsoft@gmail.com>
2022-12-07 10:01:38 +0000

    Criação do Menu

--- HorseTurfe/Program.cs
+++ HorseTurfe/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,55 @@ namespace HorseTurfe
     {
         static void Main(string[] args)
         {
+            int option;
+
+            
+
+            do{
+                Console.Clear();
+                Console.WriteLine($"\n ╔════════════════════╗" +
+                                   "\n ║        MENU        ║" +
+                                   "\n ║ 1 - Corridas       ║" +
+                                   "\n ║ 2 - Gerir Cavalos  ║" +
+                                   "\n ║ 3 - Loja           ║" +
+                                   "\n ║ 4 - Sair           ║" +
+                                   "\n ╚════════════════════╝" +
+                                   "\n\n  Escolha a sua opção: ");
+
+
+               
+                option = Int32.Parse(Console.ReadLine());
+                switch(option)
+                { 
+                case 1:
+                    Console.WriteLine("corridas");
+                break;
+                 case 2:
+                    Console.WriteLine("gerir cavalos");
+                    break;
+
+                 case 3:
+                    Console.WriteLine("loja");
+                    break;
+
+                 case 4:
+                    Console.WriteLine("Saiu do Jogo");
+                    break;
+                 
+                 default:
+                    Console.WriteLine("Opçao invalida");
+                    break;
+
+            
+            
+                 }
+                
+                Console.ReadKey(); 
+            
+            } while (option != 4);
+           
+           
+           
         }
     }
 }
