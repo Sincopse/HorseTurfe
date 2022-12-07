f08939ef603d5146d8a1991eb1af96de89de5b38
Francisco Oliveira <franciscoliveira7777@gmail.com>
2022-12-07 11:58:17 +0000

    Adicionado a classe Jockey e Team

---
+++ HorseTurfe/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseTurfe
+{
+    public class Team
+    {
+        public string Name;
+
+        public List<Horse> Horses;
+		public List<Jockey> Jockeys;
+	}
+}
