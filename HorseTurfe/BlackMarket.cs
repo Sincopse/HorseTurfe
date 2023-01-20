4412d6787e3415a6327df141813f9db16d6b4d47
Francisco Oliveira <franciscoliveira7777@gmail.com>
2023-01-20 10:06:57 +0000

    Adicionado Mercado Negro

---
+++ HorseTurfe/BlackMarket.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseTurfe
+{
+    public static class BlackMarket
+    {
+        static private Horse GenerateBMHorse()
+        {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+
+            Sex sex = (Sex)rnd.Next(2);
+
+            Breed breed = (Breed)rnd.Next(1, 4);
+
+            int age = rnd.Next(5, 16);
+            int speed = (int)(19 * Math.Sin(0.25 * age + 0.6) + rnd.Next(4, 6));
+            int control = rnd.Next(5, 16);
+            int toughness = rnd.Next(8, 17);
+            int price = rnd.Next(5001, 20000);
+
+            return new Horse(age, speed, control, toughness, price, breed, sex);
+        }
+
+        static private List<Item> items = new List<Item>()
+        {
+            new Item { Boost = 2, Name = "Saddle of Speed", Price = 6000 },
+            new Item { Boost = 4, Name = "Horseshoes of Acceleration", Price = 8000 },
+            new Item { Boost = 3, Name = "Whistle of Stamina", Price = 7000 },
+            new Item { Boost = 5, Name = "Blinkers of Focus", Price = 1000 },
+            new Item { Boost = 1, Name = "Bridle of Control", Price = 1000 }
+        };
+
+        static private List<Item> GenerateRandomItems()
+        {
+            Random rnd = new Random();
+            List<Item> generatedItems = new List<Item>();
+
+            foreach (var item in items)
+            {
+                if (rnd.Next(0, 2) == 0)
+                {
+                    generatedItems.Add(item);
+                }
+            }
+
+            return generatedItems;
+        }
+
+        static public void MercadoNegroMenu(Player player)
+        {
+            int option;
+            List<Item> availableItems = GenerateRandomItems();
+            availableItems = availableItems.Take(5).ToList();
+
+            do
+            {
+                Console.Clear();
+                UI.DrawBox("     Mercado Negro;1 - Comprar " + availableItems[0].Name + ";2 - Comprar " + availableItems[1].Name + ";3 - Comprar " + availableItems[2].Name + ";4 - Sair");
+                Console.Write("\n Escolha a sua opção: ");
+
+                if (!Int32.TryParse(Console.ReadLine(), out option))
+                {
+                    option = -1;
+                }
+
+                switch (option)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                        if (player.Money < availableItems[option - 1].Boost)
+                        {
+                            Console.WriteLine("Não tem dinheiro suficiente para comprar este item.");
+                            Console.ReadKey();
+                            break;
+                        }
+                        Console.WriteLine("Comprou " + availableItems[option - 1].Name + " por " + availableItems[option - 1].Boost + " euros");
+                        player.Money -= availableItems[option - 1].Boost;
+                        player.Items.Add(availableItems[option - 1]);
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Opçao inválida");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (option != 4);
+        }
+    }
+}
--- HorseTurfe/Calendar.cs
+++ HorseTurfe/Calendar.cs
@@ -8,26 +8,40 @@ namespace HorseTurfe
 {
     public static class Calendar
     {
-        static string month = "January";
-        static string calendar = "\n╔════════════════════╗" +
-                                 "\n║ "+month+" ░░░░░ 2023 ║" +
-                                 "\n╟──┬──┬──┬──┬──┬──┬──╢" +
-                                 "\n║░░│░░│░░│░░│░░│░░│░░║" +
-                                 "\n╟──┼──┼──┼──┼──┼──┼──╢" +
-                                 "\n║01│02│03│04│05│06│07║" +
-                                 "\n╟──┼──╔══╗──┼──┼──┼──╢" +
-                                 "\n║08│09║10║11│12│13│14║" +
-                                 "\n╟──┼──╚══╝──┼──┼──┼──╢" +
-                                 "\n║15│16│17│18│19│20│21║" +
-                                 "\n╟──┼──┼──┼──┼──┼──┼──╢" +
-                                 "\n║22│23│24│25│26│27│28║" +
-                                 "\n╟──┼──┼──┼──┼──┼──┼──╢" +
-                                 "\n║29│30│31│░░│░░│░░│░░║" +
-                                 "\n╚══╧══╧══╧══╧══╧══╧══╝";
+        static DateTime date = DateTime.Now;
+        static string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        static string[] daysOfWeek = { "Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado" };
+
+        public static string DayOfWeek()
+        {
+            return daysOfWeek[(int)date.DayOfWeek];
+        }
 
         public static void ShowCalendar()
         {
-            Console.WriteLine(calendar);
+            Console.WriteLine("\n╔════════════════════╗" +
+                             "\n║ " + months[date.Month - 1] + " ░░░░░ " + date.Year + " ║" +
+                             "\n╟──┬──┬──┬──┬──┬──┬──╢" +
+                             "\n║01│02│03│04│05│06│07║" +
+                             "\n╟──┼──┼──┼──┼──┼──┼──╢" +
+                             "\n║08│09│10│11│12│13│14║" +
+                             "\n╟──┼──┼──┼──┼──┼──┼──╢" +
+                             "\n║15│16│17│18│19│20│21║" +
+                             "\n╟──┼──┼──┼──┼──┼──┼──╢" +
+                             "\n║22│23│24│25│26│27│28║" +
+                             "\n╟──┼──┼──┼──┼──┼──┼──╢" +
+                             "\n║29│30│31│  │  │  │  ║" +
+                             "\n╚══╧══╧══╧══╧══╧══╧══╝");
+            Console.WriteLine("\nToday is " + DayOfWeek() + " " + months[date.Month - 1] + " " + date.Day + ", " + date.Year);
+
+            if (Calendar.DayOfWeek() == "Domingo")
+            {
+                Console.WriteLine("Lipem os cascos! É dia de corrida >:)");
+            }
+            if (Calendar.DayOfWeek() == "Sexta-Feira")
+            {
+                Console.WriteLine("UUUUUUuuuuUUU um  mercante chegou à cidade com descontos e itens manhosos");
+            }
         }
     }
 }
